37b2cc58f6f5b0251f1fdbf5b13363bf83e710a4
dattatreyav30 <dattatreya361@gmail.com>
2023-03-19 18:30:28 +0530

    carting

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const express = require('express');
 const shopController = require('../controllers/shop');
 
 const router = express.Router();
-
+ 
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
@@ -13,6 +13,8 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct)
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart' ,shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
